5ce1db1f9589f3221126db9e8c1dcec0b3bff85b
Thao Le <thaoabunga@thaos-mbp.attlocal.net>
2016-10-19 22:35:09 -0700

    completed kids game

--- assessment.py
+++ assessment.py
@@ -1,63 +1,235 @@
-"""
-Part 1: Discussion
+"""Dictionaries Assessment
 
-1. What are the three main design advantages that object orientation
-   can provide? Explain each concept.
-
-   1. Abstraction - Creates efficiencies by hiding details that are not required.
-   2. Encapsulation - Allows for modularity, keeps everything in one place.
-   3. Polymorphism - Enhances interchangeability of components, different things can be done within one interface.
-
-2. What is a class? A class can be thought of as guidelines for creating objects. They are often referred to as a type of thing
-like a string or file and are written in UpperCamelCase.  
-They cannot have empty bodies, a pass statement or doc strings are used so that the bodies aren't empty. Classes are also an attribute of that class.
-
-
-3. What is an instance attribute? An instance attribute is an instance on the object. They occur within specific instances of a class.
-
-4. What is a method? A method is like a function defined on a class. They must always have at least one parameter, self.
-They can also refer to attirbutes and can call other methods which is polymorhpic in nature.
-
-5. What is an instance in object orientation? An instance is a memory reference, it only has a memory address of an object in it.
-
-6. How is a class attribute different than an instance attribute?
-   Give an example of when you might use each.
-
-   The class attribute is shared by all instances, whereas, the instance attribute is specific to that instance. 
-   Class attributes are handy when storing constants, defining default values, and tracking data across all instances of a given class.
-   An instance attribute would be useful when using data attributes and methods.
+**IMPORTANT:** these problems are meant to be solved using
+dictionaries and sets.
 """
 
+def count_words(phrase):
+    """Count unique words in a string.
 
-# Parts 2 through 5:
-# Create your classes and class methods
+    This function should take a single string and return a dictionary
+    that has all of the distinct words as keys, and the number of
+    times that word appears in the string as values.
 
-class Student(object):
-	"""Student contact information"""
+    For example::
 
-	student_info = {}
-	student_info['first_name'] = first_name
-	student_info['last_name'] = last_name
-	student_info['address'] = address
+        >>> print_dict(count_words("each word appears once"))
+        {'appears': 1, 'each': 1, 'once': 1, 'word': 1}
 
-	def ___init__(self, first_name, last_name, address):
-		self.first_name = first_name
-		self.last_name = last_name
-		self.address = address
+    Words that appear more than once should be counted each time::
 
-class Question(object):
-	"""Prep questions"""
-	def ___init___(self, question, correct_answer):
-		self.question = question
-		self.correct_answer = correct_answer
-		question.ask_and_evaluate()
+        >>> print_dict(count_words("rose is a rose is a rose"))
+        {'a': 2, 'is': 2, 'rose': 3}
 
-class Exam(Question):
-	"""Exam questions"""
-	def ___init___(self, name)
-		self.name = name
-		exam.add_question(question)
-		exam.administer()
+    It's fine to consider punctuation part of a word (e.g., a comma
+    at the end of a word can be counted as part of that word) and
+    to consider differently-capitalized words as different::
 
-	if ask_and_evaluate == True:
-		score += score + 1 
+        >>> print_dict(count_words("Porcupine see, porcupine do."))
+        {'Porcupine': 1, 'do.': 1, 'porcupine': 1, 'see,': 1}
+    """
+    word_count ={} #empty dict
+    words = phrase.split() #split words in phrase
+
+    for word in phrase: #iterate word in phrase
+        word_count[word] = word_count.get(word, 0) + 1 #count words
+
+    return word_count
+
+
+def get_melon_price(melon_name):
+    """Given a melon name, return the price of the melon
+
+    Here are a list of melon names and prices:
+    Watermelon 2.95
+    Cantaloupe 2.50
+    Musk 3.25
+    Christmas 14.25
+    (it was a bad year for Christmas melons -- supply is low!)
+
+        >>> get_melon_price('Watermelon')
+        2.95
+
+        >>> get_melon_price('Musk')
+        3.25
+
+        >>> get_melon_price('Tomato')
+        'No price found'
+    """
+    melons = {'Watermelon':2.95, 
+              'Cantaloupe':2.50, 
+              'Musk': 3.25, 
+              'Christmas':14.25}
+              #melon dict
+    return melons.get(melon_name, "No price found") #use dict get method to look for melon in dict
+
+
+def word_length_sorted(words):
+    """Return list of word-lengths and words.
+
+    Given a list of words, return a list of tuples, ordered by
+    word-length. Each tuple should have two items --- a number that
+    is a word-length, and the list of words of that word length.
+
+    In addition to ordering the list by word length, order each
+    sub-list of words alphabetically.
+
+    For example::
+
+        >>> word_length_sorted(["ok", "an", "apple", "a", "day"])
+        [(1, ['a']), (2, ['an', 'ok']), (3, ['day']), (5, ['apple'])]
+    """
+    length_of_words = {} #empty dict for word
+
+    for word in words: #iterate through words list
+        word_len = len(word) #word_len determines last character of string
+        length_of_words[word_len] = length_of_words.get(word_len, []) #counts word
+        length_of_words[word_len].append(word) #append words to dict
+        length_of_words[word_len].sort() #sort words in place
+
+
+    return length_of_words.items()
+
+
+def translate_to_pirate_talk(phrase):
+    """Translate phrase to pirate talk.
+
+    Given a phrase, translate each word to the Pirate-speak
+    equivalent. Words that cannot be translated into Pirate-speak
+    should pass through unchanged. Return the resulting sentence.
+
+    Here's a table of English to Pirate translations:
+
+    ----------  ----------------
+    English     Pirate
+    ----------  ----------------
+    sir         matey
+    hotel       fleabag inn
+    student     swabbie
+    man         matey
+    professor   foul blaggart
+    restaurant  galley
+    your        yer
+    excuse      arr
+    students    swabbies
+    are         be
+    restroom    head
+    my          me
+    is          be
+    ----------  ----------------
+
+    For example::
+
+        >>> translate_to_pirate_talk("my student is not a man")
+        'me swabbie be not a matey'
+
+    You should treat words with punctuation as if they were different
+    words::
+
+        >>> translate_to_pirate_talk("my student is not a man!")
+        'me swabbie be not a man!'
+    """
+    eng_to_pirate = {'sir':'matey','hotel':'fleabag inn','student':'swabbie','man':'matey','professor':'foul blaggart','retaurant':'galley',
+    'your':'yer','excuse':'arr','students':'swabbies','are':'be','restroom':'head','my':'me','is':'be'}
+    #created dict
+
+    translation = [] #empty list to append translation
+
+    for word in phrase.split(): #split phrase
+        word = eng_to_pirate.get(word, word) #count word
+        translation.append(word)
+
+    return " ".join(translation)
+
+
+def kids_game(names):
+    """Play a kids' word chain game.
+
+    Given a list of names, like::
+
+      bagon baltoy yamask starly nosepass kalob nicky
+
+    Do the following:
+
+    1. Always start with the first word ("bagon", in this example).
+
+    2. Add it to the results.
+
+    3. Use the last letter of that word to look for the next word.
+       Since "bagon" ends with n, find the *first* word starting
+       with "n" in our list --- in this case, "nosepass".
+
+    4. Add "nosepass" to the results, and continue. Once a word has
+       been used, it can't be used again --- so we'll never get to
+       use "bagon" or "nosepass" a second time.
+
+    5. When you can't find an unused word to use, you're done!
+       Return the list of output words.
+
+    For example::
+
+        >>> kids_game(["bagon", "baltoy", "yamask", "starly",
+        ...            "nosepass", "kalob", "nicky", "booger"])
+        ['bagon', 'nosepass', 'starly', 'yamask', 'kalob', 'baltoy']
+
+    (After "baltoy", there are no more y-words, so we end, even
+    though "nicky" and "booger" weren't used.)
+
+    Another example:
+
+        >>> kids_game(["apple", "berry", "cherry"])
+        ['apple']
+
+    This is a tricky problem. In particular, think about how using
+    a dictionary (with the super-fast lookup they provide) can help;
+    good solutions here will definitely require a dictionary.
+    """
+
+    possible_letter_to_words = {}
+
+    results = [names.pop(0)]
+
+    for name in names:
+        if name[0] not in possible_letter_to_words:
+
+        else:
+            possible_letter_to_words[name[0].append(name)] #creating a dictionary of possible letters 
+
+    while True: #create markov chain
+        
+        start_letter = output[-1][-1]
+
+        if not possible_letter_to_words(start_letter):
+            break
+
+        word = possible_letter_to_words[start_letter].pop(0)
+        results.append(word)
+
+    return results
+
+
+#####################################################################
+# You can ignore everything below this.
+
+def print_dict(d):
+    # This method is used to print dictionaries in key-alphabetical
+    # order, and is only for our doctests. You can ignore it.
+    if isinstance(d, dict):
+        print "{" + ", ".join(
+            "%r: %r" % (k, d[k]) for k in sorted(d)) + "}"
+    else:
+        print d
+
+
+def sort_pairs(l):
+    # Print sorted list of pairs where the pairs are sorted. This is
+    # used only for documentation tests. You can ignore it.
+    return sorted(sorted(pair) for pair in l)
+
+
+if __name__ == "__main__":
+    print
+    import doctest
+    if doctest.testmod().failed == 0:
+        print "*** ALL TESTS PASSED ***"
+    print
